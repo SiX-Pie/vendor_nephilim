GhostProjectRoms/vendor_six

changes to build six rom ***WIP***

tools/changelog.sh
@@ -1,0 +1,11 @@
+#!/bin/sh
+
+# Exports
+. $ANDROID_BUILD_TOP/vendor/six/tools/colors
+
+export Changelog=Changelog.txt
+
+sed -i 's/project/   */g' $Changelog
+
+cp $Changelog $OUT/system/etc/
+cp $Changelog $OUT/

SiX-Pie/vendor_nephilim

tools/changelog.sh
@@ -1,0 +1,11 @@
+#!/bin/sh
+
+# Exports
+. $ANDROID_BUILD_TOP/vendor/gzosp/tools/colors
+
+export Changelog=Changelog.txt
+
+sed -i 's/project/   */g' $Changelog
+
+cp $Changelog $OUT/system/etc/
+cp $Changelog $OUT/