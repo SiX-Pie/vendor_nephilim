889679702cd36de45093096d4da11b0e1205dfa1
R3Ds <fpoolboy@gmail.com>
2017-12-21 12:23:51 -0500

    shamu: Update to 8.1.0 OPM2.171019.012

--- configs/version.mk
+++ configs/version.mk
@@ -14,7 +14,7 @@
 
 #SiX versioning
 ifndef SIX_BUILD_TYPE
-    SIX_BUILD_TYPE := OPM1-BETA5-R3Ds
+    SIX_BUILD_TYPE := OPM2-BETA5-R3Ds
 endif
 
 SIX_VERSION := $(PLATFORM_VERSION)-$(shell date +%Y%m%d)-$(SIX_BUILD_TYPE)
